0d5a76f03b4df0d9efeca2de9a67c2800bbe252d
Alejandromendivelso <amendivels3@uniminuto.edu.co>
2020-03-11 19:49:49 -0500

    do

--- Busqueda_Binaria.py
+++ Busqueda_Binaria.py
@@ -4,37 +4,33 @@ import time
 Archivo = open('Numeros.txt','w')
 newText=''
 for numero in range(10):
-    newText += str(random.randrange(199)) + ','
+    newText += str(random.randrange(20)) + ','
 Archivo.write(newText.strip(','))
 
 Archivo.close()
+
+def busqueda_binaria(n_lista, valores):
+    primero =0
+    ultimo = len(n_lista) -1
+    encon = False
+
+    while primero <= ultimo and not encon:
+         mitad = (primero + ultimo)//2
+
+         if n_lista[mitad] ==valores:
+             encon= True
+         else:
+            if valores <  n_lista[mitad]:
+                ultimo =mitad -1
+            else:
+                primero = mitad+1
+    return encon
 
 contenido=open('Numeros.txt','r')
 listas= [lista.split() for lista in contenido]
 for lista in listas:
     print  (listas)
-
-def busqueda_binaria(lista,valor):
-    primero = 0
-    ultimo = len(lista)-1
-    encontro = False
-
-    while primero <= ultimo and not encontro:
-        mitad =(primero + ultimo)// 2
-
-        if lista[mitad] == valor:
-            encontro = True
-        else:
-            if valor < lista[mitad]:
-                ultimo = mitad -1
-            else:
-                primero = mitad +1
-    return encontro
-
-    listas = []
-
-    
-
-
-
+n_lista = sorted(listas)
+llave = 3
+print(busqueda_binaria(n_lista,llave))
 
